0d14e414ab77365375930c8ca458ea139645b617
Ricardo <ricardo270298@gmail.com>
2024-11-04 10:46:38 -0400

    - Workflow updated - Vite configuration updated, base url added for build process

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig(({ command }) => {
+//   if (command === "build") {
+//     return {
+//       plugins: [react()],
+//       base: "https://ricandres98.github.io/copy-maker/"
+//     }
+//   } else if (command === "serve" || command ==="dev") {
+//     return {
+//       plugins: [react()],
+//     }
+//   }
+// })
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      plugins: [react()],
+    };
+  } else {
+    return {
+      plugins: [react()],
+      base: "https://ricandres98.github.io/copy-maker/",
+    };
+  }
+});
